61536417def038c0bf1e9b713a65b87ff2f92e71
Rye4000 <161626949+Rye4000@users.noreply.github.com>
2024-05-29 14:04:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import turtle
+
+#Change the background color to black
+turtle.bgcolor("black")
+
+#Adjust the speed of turtle
+turtle.speed(2)
+
+#List of colors
+colors = ["#00ff9f", "#00b8ff", "#001eff", "#bd00ff", "#fcee0c"]
+
+#Counter variables
+colorNum = 0
+size = 18
+
+#Change this variable to control how many sides your shape will have
+sides = 8
+#Draw the square shape
+
+#Draw the spiraling shape
+#This loops forever
+while (True):
+  colorNum = colorNum + 1
+  for i in range(sides):
+    turtle.forward(size)
+    turtle.right(360 / sides + .1)
+    size = size + 1
+  turtle.color(colors[colorNum % 5])
